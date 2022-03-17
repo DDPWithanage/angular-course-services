ebf6680065e35e95454d7e6ef6561d3f6fe59112
DuliniDilmini <ddpwithanage@gmail.com>
2022-03-17 23:49:39 +0530

    Creating services

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {COURSES} from '../db-data';
+import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
+import {Observable} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { CoursesService } from './services/courses.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  courses$ : Observable<Course[]>;
+
+  constructor(private courseService: CoursesService) {
+
+  }
+
+  ngOnInit() {
+    
+    const params = new HttpParams()
+      .set("page", "1")
+      .set("pageSize", "10");
+
+    this.courses$ = this.courseService.loadCourses();
+
+  }
+
+
+
+}
