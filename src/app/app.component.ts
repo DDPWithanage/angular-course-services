32d721ce41fef6fc1975347b35b7e7afe99e1099
DuliniDilmini <ddpwithanage@gmail.com>
2022-03-18 18:21:53 +0530

    Create Angular custom service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { CoursesService } from './services/courses.service';
 
 @Component({
@@ -28,6 +28,14 @@ export class AppComponent implements OnInit {
 
     this.courses$ = this.courseService.loadCourses();
 
+  }
+
+  save(course: Course){
+    
+    const headers = new HttpHeaders();
+    this.courseService.saveCourse(course).subscribe(
+      () => console.log('Course saved!')
+    );
   }
 
 
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from '../model/course';
@@ -17,6 +17,13 @@ export class CoursesService {
     .set("page", "1")
     .set("pageSize", "10");
 
-  return this.http.get<Course[]>('api/courses', {params});
+    return this.http.get<Course[]>('api/courses', {params});
+   }
+
+   saveCourse(course:Course){
+     const headers = new HttpHeaders()
+      .set("X-Auth", "userId");
+
+     return this.http.put(`/api/courses/${course.id}`, course, {headers});
    }
 }
